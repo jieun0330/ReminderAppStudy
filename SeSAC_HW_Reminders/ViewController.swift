5ebe08d2d4ed388f7ce46e2bee31ec78950602b5
jieun <jieun_0330@naver.com>
2024-02-14 15:00:48 +0900

    :tada: [init] inital commit

---
+++ SeSAC_HW_Reminders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_HW_Reminders
+//
+//  Created by 박지은 on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
