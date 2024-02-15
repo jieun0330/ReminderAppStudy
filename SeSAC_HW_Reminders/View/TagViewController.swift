675e1cef45015c78ee51fe4d52eda575c8da8edd
jieun <jieun_0330@naver.com>
2024-02-15 17:51:29 +0900

    :sparkles: [feat] 태그 값전달 notificationCenter 이용

---
+++ SeSAC_HW_Reminders/View/TagViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TagViewController.swift
+//  SeSAC_HW_Reminders
+//
+//  Created by 박지은 on 2/15/24.
+//
+
+import UIKit
+import SnapKit
+
+class TagViewController: BaseViewController {
+    
+    lazy var textField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "새로운 태그 추가..."
+        field.delegate = self
+        field.backgroundColor = .lightGray
+        return field
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func configureHierarchy() {
+        [textField].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func configureConstraints() {
+        textField.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.height.equalTo(40)
+        }
+    }
+    
+    override func configureView() {
+        view.backgroundColor = .white
+        navigationItem.title = "태그"
+    }
+
+}
+
+extension TagViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // 1. 텍스트필드에 입력값을 다 마치면 notificationCenter 통해서 값을 전달해줄거니까 우선 저장해보자
+        NotificationCenter.default.post(name: Notification.Name("TextFieldReceived"),
+                                        object: nil,
+                                        userInfo: ["textField": textField.text!])
+    }
+}
--- SeSAC_HW_Reminders/View/ToDoViewController.swift
+++ SeSAC_HW_Reminders/View/ToDoViewController.swift
@@ -21,13 +21,24 @@ class ToDoViewController: BaseViewController {
         return view
     }()
         
-    var receivedDate = "받아온 날짜"
+    var receivedDate = ""
+    var receivedTextField = ""
     
     var memoList: [String] = ["제목", "메모"]
     var todoList: [String] = ["마감일", "태그", "우선 순위", "이미지 추가"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // notificationCenter를 ViewWillAppear가 아닌 viewDidLoad에 쓰는 이유
+        // 얘는 나 값 받을거야~ 설정~! 이라서
+        // viewDidLoad때 설정을 뙇 처음 한번 해주는거임
+        // viewWillAppear에서 설정을 해버리면 화면이 바뀔때마다 설정설정설정섲엊섲 하겠지?
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(tagReceivedNotification),
+                                               name: Notification.Name(rawValue: "TextFieldReceived"),
+                                               object: nil)
+        
     }
     
     override func configureHierarchy() {
@@ -45,6 +56,18 @@ class ToDoViewController: BaseViewController {
     override func configureView() {
         view.backgroundColor = .white
         navigationItem.title = "새로운 할 일"
+    }
+    
+    @objc func tagReceivedNotification(notification: NSNotification) {
+        // nsnotification.userinfo에 저장된 딕셔너리 값을 가져오기 위해
+        // value는 Any?값이 된다 -> 타입캐스팅을 해준다
+        // String? -> 옵셔널 바인딩을 해준다
+        if let value = notification.userInfo?["textField"] as? String {
+            // 이것도 cell에 넣어줘야하니까 변수를 만들어주자
+            receivedTextField = value
+            // 이것도 reload?
+            tableView.reloadData()
+        }
     }
 }
 
@@ -71,19 +94,32 @@ extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: ToDoTableViewCell.identifier, for: indexPath) as! ToDoTableViewCell
+            
+            cell.title.text = todoList[indexPath.section-1]
+            
+            if indexPath.section == 1 {
+                cell.receivedTitle.text = receivedDate
+
+            } else {
+                cell.receivedTitle.text = receivedTextField
+            }
+            
             // 5. receivedDate에 담아줬으니까 cell에 보여줄수 있게 되었어!!
-            cell.receivedTitle.text = receivedDate
+            
         
             return cell
         }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath)
+
         
         if indexPath.section == 1 {
             let vc = DateViewController()
             navigationController?.pushViewController(vc, animated: true)
             
+            // 1️⃣ 클로저 방법
             // 4. 가져온걸 cell에다가 쓰려면 변수에 담아줘야할거같아
             vc.selectedDate = {value in
                 self.receivedDate = value
@@ -91,6 +127,10 @@ extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
                 tableView.reloadData()
             }
 
+            
+        } else if indexPath.section == 2 {
+            let vc = TagViewController()
+            navigationController?.pushViewController(vc, animated: true)
             
         } else {
             
